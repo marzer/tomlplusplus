71b57a323867e9ea532f1a7c8c4239c5cfb0dbc5
Mark Gillard <marzer_@hotmail.com>
2022-01-13 01:24:09 +0200

    added regression test to detect ODR violations

---
+++ tests/odr_test_1.cpp
@@ -0,0 +1,8 @@
+#define TOML_ENABLE_UNRELEASED_FEATURES 1
+#define TOML_HEADER_ONLY				1
+#include "../toml.hpp"
+
+int main()
+{
+	return 0;
+}
---
+++ tests/odr_test_2.cpp
@@ -0,0 +1,3 @@
+#define TOML_ENABLE_UNRELEASED_FEATURES 1
+#define TOML_HEADER_ONLY				1
+#include "../toml.hpp"
