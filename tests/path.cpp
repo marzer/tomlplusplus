fb8ce80350bcde1d109e9b4d7a571bbc2f29a8bd
Alexey Ismagilov <38747776+a-is@users.noreply.github.com>
2022-09-19 02:52:48 +0300

    Fix typo in at_path (#173)
    * ENH: tests: replacing tabs with '\t'.
    
    * NEW: tests: adding tests for `path`
    
    Added the test that detects an error in the code.
    Code fixing in the following commit.
    
    * FIX: at_path: typo in `parse_path`
    
    Fixing bug in the code. Also fixing `path - parsing` tests.
    
    * NEW: readme: update list of contributors

--- tests/path.cpp
+++ tests/path.cpp
@@ -16,7 +16,8 @@ TEST_CASE("path - parsing")
 		CHECK(toml::path("  [1][2]").str() == "  [1][2]");
 		CHECK(toml::path("a.  .b").str() == "a.  .b");
 		CHECK(toml::path("test[23]").str() == "test[23]");
-		CHECK(toml::path("[ 120		]").str() == "[120]");
+		CHECK(toml::path("[ 120  ]").str() == "[120]");
+		CHECK(toml::path("[ 120\t\t]").str() == "[120]");
 		CHECK(toml::path("test.value").str() == "test.value");
 		CHECK(toml::path("test[0].value").str() == "test[0].value");
 		CHECK(toml::path("test[1][2]\t .value").str() == "test[1][2].value");
@@ -31,7 +32,8 @@ TEST_CASE("path - parsing")
 		CHECK(toml::path(L"  [1][2]").str() == "  [1][2]");
 		CHECK(toml::path(L"a.  .b").str() == "a.  .b");
 		CHECK(toml::path(L"test[23]").str() == "test[23]");
-		CHECK(toml::path(L"[ 120		]").str() == "[120]");
+		CHECK(toml::path(L"[ 120  ]").str() == "[120]");
+		CHECK(toml::path(L"[ 120\t\t]").str() == "[120]");
 		CHECK(toml::path(L"test.value").str() == "test.value");
 		CHECK(toml::path(L"test[0].value").str() == "test[0].value");
 		CHECK(toml::path(L"test[1][2]\t .value").str() == "test[1][2].value");
