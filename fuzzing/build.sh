625ab0252529e188fec82ef737fa1dd72e629228
bcapuano <bcapuano@asu.edu>
2023-11-16 22:03:38 -0500

    Integration into build-system and initial harness

---
+++ fuzzing/build.sh
@@ -0,0 +1,8 @@
+cd $SRC/tomlplusplus
+mkdir -p build
+cmake -S . -B build -DBUILD_FUZZER=ON && cmake --build build --target install
+
+# Build the corpus using the existing toml files in the source
+mkdir -p corpus
+find $SRC/tomlplusplus -name "*.toml" -exec cp {} corpus \;
+zip -q $OUT/toml_fuzzer_seed_corpus.zip corpus/*
---
+++ fuzzing/toml_fuzzer.cpp
@@ -0,0 +1,41 @@
+#include <cstdint>
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include <toml++/toml.hpp>
+
+enum class SerializationTest
+{
+    NONE = 0,
+    JSON,
+    YAML,
+    TOML,
+    kMaxValue = YAML
+};
+
+extern "C" int LLVMFuzzerTestOneInput(const std::uint8_t* data, const std::size_t size)
+{
+    FuzzedDataProvider fdp{data, size};
+    try
+    {
+        const toml::table tbl = toml::parse(fdp.ConsumeRandomLengthString());
+
+        switch (fdp.ConsumeEnum<SerializationTest>())
+        {
+            case SerializationTest::JSON:
+                static_cast<void>(toml::json_formatter{tbl});
+                break;
+            case SerializationTest::YAML:
+                static_cast<void>(toml::yaml_formatter{tbl});
+                break;
+            case SerializationTest::TOML:
+                static_cast<void>(toml::toml_formatter{tbl});
+            default:
+                break;
+        }
+    }
+    catch (const toml::parse_error&)
+    {
+        return -1;
+    }
+    return 0;
+}
